68fba23a3b8bce2e50e7482f2ce4c537ffbfeb52
xuwd11 <31458899+xuwd11@users.noreply.github.com>
2018-04-25 22:56:55 -0400

    added co2_regression_pytorch

---
+++ co2_regression/co2_regression_MC_dropout_tanh_pytorch.py
@@ -0,0 +1,106 @@
+import numpy as np
+import time
+import h5py
+
+import torch
+import torchvision
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+import pickle
+
+class IO:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        
+    def to_pickle(self, obj):
+        with open(self.file_name, 'wb') as output:
+            pickle.dump(obj, output, protocol=pickle.HIGHEST_PROTOCOL)
+    
+    def read_pickle(self):
+        with open(self.file_name, 'rb') as input_:
+            obj = pickle.load(input_)
+        return obj
+    
+f = h5py.File('data/train.h5', 'r')
+data_train = np.concatenate((f['data'].value, f['label'].value), axis=1)
+f.close()
+X_train = data_train[:, 0].reshape(-1, 1)
+y_train = data_train[:, 1].reshape(-1, 1)
+print(data_train.shape)
+
+f = h5py.File('data/test.h5', 'r')
+data_test = np.concatenate((f['data'].value, f['label'].value), axis=1)
+f.close()
+
+X_test = np.arange(-1.72, 3.51, 0.01).reshape(-1, 1)
+
+class MLP2(nn.Module):
+    def __init__(self, hidden_layers=[1024, 1024, 1024, 1024], droprate=0.1, activation='relu'):
+        super(MLP2, self).__init__()
+        self.model = nn.Sequential()
+        self.model.add_module('input', nn.Linear(1, hidden_layers[0]))
+        if activation == 'relu':
+            self.model.add_module('relu0', nn.ReLU())
+        elif activation == 'tanh':
+            self.model.add_module('tanh0', nn.Tanh())
+        for i in range(len(hidden_layers)-1):
+            self.model.add_module('dropout'+str(i+1), nn.Dropout(p=droprate))
+            self.model.add_module('hidden'+str(i+1), nn.Linear(hidden_layers[i], hidden_layers[i+1]))
+            if activation == 'relu':
+                self.model.add_module('relu'+str(i+1), nn.ReLU())
+            elif activation == 'tanh':
+                self.model.add_module('tanh'+str(i+1), nn.Tanh())
+        self.model.add_module('dropout'+str(i+2), nn.Dropout(p=droprate))
+        self.model.add_module('final', nn.Linear(hidden_layers[i+1], 1))
+        
+    def forward(self, x):
+        return self.model(x)
+    
+class MLP2Regressor:
+    def __init__(self, hidden_layers=[1024, 1024, 1024, 1024], droprate=0.1, activation='relu', \
+                 max_epoch=1000000, lr=0.0001):
+        self.max_epoch = max_epoch
+        self.lr = lr
+        self.model = MLP2(hidden_layers=hidden_layers, droprate=droprate, activation=activation)
+        self.model.cuda()
+        self.criterion = nn.MSELoss().cuda()
+        self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
+        
+    def fit(self, X_train, y_train, verbose=True):
+        X = Variable(torch.from_numpy(X_train).type(torch.FloatTensor)).cuda()
+        y = Variable(torch.from_numpy(y_train).type(torch.FloatTensor)).cuda()
+        print(self.model)
+        for epoch in range(self.max_epoch):
+            self.optimizer.zero_grad()
+            outputs = self.model(X)
+            loss = self.criterion(outputs, y)
+            loss.backward()
+            self.optimizer.step()
+            if verbose:
+                print('Epoch {} loss: {}'.format(epoch+1, loss.data[0]))
+        return self
+    
+def predict(model, X, T=10000):
+    X = Variable(torch.from_numpy(X).type(torch.FloatTensor)).cuda()
+    model = model.train()
+    Yt_hat = np.array([model(X).data.cpu().numpy() for _ in range(T)]).squeeze()
+    model = model.eval()
+    y_pred = model(X).data.cpu().numpy()
+    model = model.train()
+    return y_pred, Yt_hat
+
+start = time.time()
+mlp2 = MLP2Regressor(activation='tanh').fit(X_train, y_train, verbose=True)
+torch.save(mlp2.model, 'results/co2_regression_MC_tanh_pytorch')
+print('Time used: {}'.format(time.time() - start))
+
+start = time.time()
+model = torch.load('results/co2_regression_MC_tanh_pytorch')
+y_pred, y_hat = predict(model, X_test)
+y_mc = y_hat.mean(axis=0)
+y_mc_std = y_hat.std(axis=0)
+IO('results/co2_regression_MC_tanh_pytorch_results.pkl').to_pickle((y_pred, y_mc, y_mc_std))
+print('Time used: {}'.format(time.time() - start))
